28d72d97b451f6d62a5b22e3e6b894533b4a085f
chelsea roberts <chelsea.j.roberts@hotmail.com>
2022-12-07 09:52:44 +0000

    first commit

--- app/controllers/produce_controller.rb
+++ app/controllers/produce_controller.rb
@@ -1,2 +1,14 @@
 class ProduceController < ApplicationController
+  # before_action :find_produce, only: %w[show]
+
+  def show
+
+  end
+
+  private
+
+  # def find_produce
+  #   @produce = Produce.find(params[:id])
+  # end
+
 end
