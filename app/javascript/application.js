5750c957f74b868752e7025c99ff511addbfa725
Constanze Meyer <constanze.xyz@gmail.com>
2022-12-07 10:31:17 +0100

    add picture banner

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+import { calendarControl } from "./controllers/calendar"
+
+//check the console for date click event
+//Fixed day highlight
+//Added previous month and next month view
+calendarControl()
