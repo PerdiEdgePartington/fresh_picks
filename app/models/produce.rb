548c431599b5a44f2f611fc5dac2c6512b31f793
Constanze Meyer <constanze.xyz@gmail.com>
2022-12-09 11:04:43 +0100

    edit PGsearch

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -4,14 +4,4 @@ class Produce < ApplicationRecord
   has_many :produce_recipes
   has_many :recipes, through: :produce_recipes
   has_one_attached :photo
-
-  include PgSearch::Model
-  pg_search_scope :global_search,
-    against: [ :name ],
-    associated_against: {
-      produce_seasons: [ :months ]
-    },
-    using: {
-      tsearch: { prefix: true }
-    }
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -1,4 +1,15 @@
 class Season < ApplicationRecord
   has_many :produce_seasons
   has_many :produce, through: :produce_seasons
+
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name ],
+    associated_against: {
+      produce_seasons: [ :produce_id ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
