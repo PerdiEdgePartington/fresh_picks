4b043ca45138ef0bb7b54521bac13e39b5f8a711
Constanze Meyer <constanze.xyz@gmail.com>
2022-12-12 10:50:53 +0100

    add pg search to produce model

--- Gemfile
+++ Gemfile
@@ -58,7 +58,7 @@ gem "simple_form", github: "heartcombo/simple_form"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
-  
+
 
 end
 
--- app/models/produce.rb
+++ app/models/produce.rb
@@ -4,4 +4,11 @@ class Produce < ApplicationRecord
   has_many :produce_recipes
   has_many :recipes, through: :produce_recipes
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_season_id,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
