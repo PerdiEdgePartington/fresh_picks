5d38b64a347e47ef6e3b99d772b4cd17efe291a8
chelsea roberts <chelsea.j.roberts@hotmail.com>
2022-12-13 10:12:50 +0000

    first commit

--- app/controllers/produce_controller.rb
+++ app/controllers/produce_controller.rb
@@ -2,6 +2,12 @@ class ProduceController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
     @produce = Produce.all
+
+    if params[:query].present?
+    @produce = Produce.where("name ILIKE ?", "%#{params[:query]}%")
+  else
+    @produce = Produce.all
+  end
   end
 
   def show
